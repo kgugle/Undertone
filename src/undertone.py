bab8dffea5505b8241218cdb958dcc67927c6be5
Karan Gugle <kgugle@cmu.edu>
2016-07-02 19:59:26 -0700

    Add files via upload
    added california() function, which gives percentiles from z-scores from total scores per county of seafood

--- src/undertone.py
+++ src/undertone.py
@@ -4,6 +4,8 @@ from pprint import pprint
 import re
 import time
 import math
+import numpy as np
+from scipy import stats
 
 auth = Oauth1Authenticator(
     consumer_key='',
@@ -162,14 +164,26 @@ def handle_responses(location_arr):
 		query = location_instance.neighborhood + ', ' + location_instance.city + ', ' + location_instance.state
 	else:
 		query = location_instance.city + ', ' + location_instance.state
-	response = client.search(query, **params)
-	response_2 = client.search(query, **params_offset)
+	try:
+		response = client.search(query, **params)
+	except:
+		response = []
+	try:
+		response_2 = client.search(query, **params_offset)
+	except:
+		response_2 = []
 
 	first_20, second_20 = [],[]
-	for x in range(0, len(response.businesses)): #1st page
-		first_20.append([response.businesses[x].id,response.businesses[x].location.city]) #append [Yelp Business ID, City of Business]
-	for x in range(0, len(response_2.businesses)): #2nd page
-		second_20.append([response_2.businesses[x].id,response_2.businesses[x].location.city]) 
+	if response == []:
+		first_20 = []
+	else:
+		for x in range(0, len(response.businesses)): #1st page
+			first_20.append([response.businesses[x].id,response.businesses[x].location.city]) #append [Yelp Business ID, City of Business]
+	if response_2 == []:
+		second_20 = []
+	else:
+		for x in range(0, len(response_2.businesses)): #2nd page
+			second_20.append([response_2.businesses[x].id,response_2.businesses[x].location.city]) 
 	all_results = first_20 + second_20 #combine first page and second page results
 	city_restaurants = [] #will hold Yelp Business ID's 
 
@@ -200,6 +214,7 @@ def rate(business_id_array):
 		#go through all the restaurants in the city
 		for business_id in business_id_array:
 			response = client.get_business(business_id, **params)
+
 			try:
 				#exponent is LOG(review_count), look at the project's paper for an explanation
 				exponent = math.log(response.business.review_count)
@@ -218,12 +233,54 @@ def rate(business_id_array):
 			score += total
 		return score
 
+def percentage_of_area_under_std_normal_curve_from_zcore(z_score):
+    return .5 * (math.erf(z_score / 2 ** .5) + 1)
+
+def california():
+	calif = []
+	with open('california.txt') as f:
+		calif = f.readlines()
+	out_array = []
+	for ccs in calif:
+		cal_array = ccs.split(',')
+		piece = [cal_array[0].replace('"',''),cal_array[1].replace(' ',''),cal_array[2].replace(' ','').replace('\n','')]
+		out_array.append(piece)
+	array_county = []
+	for x in out_array:
+		array_county.append(x[0])
+	set_array_county = set(array_county)
+	final_arr = []
+	for county in set_array_county:
+		divisor = 0
+		total_score = 0
+		for y in out_array:
+			if county == y[0]:
+				divisor += int(y[1])
+				if divisor == 0:
+					divisor = 1
+				total_score += float(y[2])
+		final_arr.append((total_score/divisor))
+	pprint(final_arr)
+	np_array = np.asarray(final_arr)
+	zscore_arr = stats.zscore(np_array)
+
+	fin = []
+	for x in zscore_arr:
+		num = percentage_of_area_under_std_normal_curve_from_zcore(x)
+		fin.append(num)
+
+	proj = zip(set_array_county,fin)
+	for x in proj:
+		print(x[0],x[1])
+
+
+
 def main():
 	info_array_f = import_and_hash()
 	file = open("UNDERTONE-1.txt", "w")
-	counter = 0
-	for location in info_array_f:
-		line_cr = handle_responses(location)
+	counter = 2702
+	for x in range(2702,len(info_array_f)):
+		line_cr = handle_responses(info_array_f[x])
 		#pprint(line_cr)
 
 		score = rate(line_cr[3])
@@ -231,12 +288,14 @@ def main():
 		num = line_cr[1]
 		#add num to file so it can be used later for averages
 		final_write = [county, num, str(score)]
-		pprint(final_write)
+		pprint(str(counter) + '      ' + str(final_write))
 
 		file.write(', '.join([str(x) for x in final_write]))
 		file.write('\n')
+		#print(counter)
 		counter+=1
 	file.close()
+	california()
 
 start_time = time.time()
 main()
